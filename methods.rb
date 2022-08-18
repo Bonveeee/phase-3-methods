8aac8ad70d71fa50fad12eb893193657fed1c333
bonveeee <bonochieng@gmail.com>
2022-08-18 17:41:41 +0300

    commit tree 2d3d4457dd6604623acc840c0a0aa626c6d3303a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    "Hello, programmer!"
+end
+
+
+
+def greet(name)
+     return "Hello, #{name}!"
+end
+
+puts greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    "Hello, #{name}!"
+end
+ puts greet_with_default("Jimmy")
+
+ def add(num1, num2)
+    return num1 + num2
+ end
+
+ puts add(1, 3)
+
+def halve(num)
+    if num.class != Integer
+        return nil    
+end
+    num/2
+end
+
+puts halve("block")
+  
